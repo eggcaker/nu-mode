c2df8b08b7f5a31d423f8b21a472fc95ab95ae10
eggcaker <eggcaker@gmail.com>
2022-02-15 22:16:59 +0800

    add nushell commands font lock face

--- nu-mode.el
+++ nu-mode.el
@@ -26,52 +26,72 @@
 (add-to-list 'auto-mode-alist '("\\.nu\\'" . nu-mode))
 
 
+(defconst nu-types '("int" "string" "float" "duration"))
+(defconst nu-keywords '("let" "for" "in" "if" "else" "elseif"))
+(defconst nu-constants '("PATH"))
 
-(defcustom nu-command-builtins '(
-                                 ("core" . '("alias" "debug" "def" "def-env" "describe" "do" "du" "echo" "error make" "export" "export def" "export def-env" "export env" "extern" "for" "help" "hide" "history" "if" "ignore" "let" "metadata" "module" "register" "source" "tutor" "use" "view-source"))
-                                 ("filters" . '("all?" "any?" "append" "collect" "columns" "compact" "default" "drop" "drop column" "drop nth" "each" "each group" "each window" "empty?" "every" "find" "first" "flatten" "get" "headers" "keep" "keep until" "keep while" "last" "length" "lines" "merge" "move" "par-each" "par-each group" "prepend" "query" "query json" "query xml" "range" "reject" "rename" "reverse" "rotate" "select" "shuffle" "skip" "skip until" "skip while" "sort-by" "uniq" "update" "update cells" "where" "wrap" "zip"))
-                                 ("platform" . '("ansi" "ansi gradient" "ansi strip" "clear" "input" "keybindings" "keybindings default" "keybindings list" "keybindings listen" "kill" "sleep" "term size"))
-                                 ("system" . '("benchmark" "exec" "ps" "run_external" "sys" "which"))
-                                 ("strings" . '("build-string" "char" "decode" "detect columns" "format" "nu-highlight" "parse" "size" "split" "split chars" "split column" "split row" "str" "str camel-case" "str capitalize" "str collect" "str contains" "str downcase" "str ends-with" "str find-replace" "str index-of" "str kebab-case" "str length" "str lpad" "str pascal-case" "str reverse" "str rpad" "str screaming-snake-case" "str snake-case" "str starts-with"))
-                                 ("generators" . '("cal" "seq" "seq date"))
-                                 ("filesystem" . '("cd" "cp" "load-env" "ls" "mkdir" "mv" "open" "rm" "save" "touch"))
-                                 ("deprecated" . '("dataframe" "insert" "nth" "pivot" "str to-decimal" "str to-int"))
-                                 ("date" . '("date" "date format" "date humanize" "date list-timezone" "date now" "date to-table" "date to-timezone"))
-                                 ("dataframe" . '("dfr" "dfr aggregate" "dfr all-false" "dfr all-true" "dfr append" "dfr arg-max" "dfr arg-min" "dfr arg-sort" "dfr arg-true" "dfr arg-unique" "dfr column" "dfr concatenate" "dfr contains" "dfr count-null" "dfr count-unique" "dfr cumulative" "dfr describe" "dfr drop" "dfr drop-nulls" "dfr dtypes" "dfr filter-with" "dfr first" "dfr get" "dfr get-day" "dfr get-hour" "dfr get-minute" "dfr get-month" "dfr get-nanosecond" "dfr get-ordinal" "dfr get-second" "dfr get-week" "dfr get-weekday" "dfr get-year" "dfr group-by" "dfr is-duplicated" "dfr is-in" "dfr is-not-null" "dfr is-null" "dfr is-unique" "dfr join" "dfr last" "dfr melt" "dfr not" "dfr open" "dfr pivot" "dfr rename" "dfr rename-col" "dfr replace" "dfr replace-all" "dfr rolling" "dfr sample" "dfr set" "dfr set-with-idx" "dfr shape" "dfr shift" "dfr slice" "dfr sort" "dfr str-lengths" "dfr str-slice" "dfr strftime" "dfr take" "dfr to-csv" "dfr to-df" "dfr to-dummies" "dfr to-lowercase" "dfr to-nu" "dfr to-parquet" "dfr to-uppercase" "dfr unique" "dfr value-counts" "dfr with-column"))
-                                 ("shells" . '("enter" "exit" "g" "n" "p" "shells"))
-                                 ("env" . '("env" "let-env" "with-env"))
-                                 ("network" . '("fetch" "query web" "url" "url host" "url path" "url query" "url scheme"))
-                                 ("conversions" . '("fmt" "into" "into binary" "into bool" "into datetime" "into filesize" "into int" "into string"))
-                                 ("formats" . '("from" "from csv" "from eml" "from ics" "from ini" "from json" "from ods" "from ssv" "from toml" "from tsv" "from url" "from vcf" "from xlsx" "from xml" "from yaml" "from yml" "to" "to csv" "to html" "to json" "to md" "to toml" "to tsv" "to url" "to xml" "to yaml"))
-                                 ("default" . '("git checkout" "git push" "group-by" "hash md5" "hash sha256" "into decimal" "path" "path basename" "path dirname" "path exists" "path expand" "path join" "path parse" "path relative-to" "path split" "path type" "reduce" "split-by" "str substring" "str trim" "str upcase" "transpose" "version"))
-                                 ("viewers" . '("grid" "table"))
-                                 ("hash" . '("hash" "hash base64"))
-                                 ("math" . '("math" "math abs" "math avg" "math ceil" "math eval" "math floor" "math max" "math median" "math min" "math mode" "math product" "math round" "math sqrt" "math stddev" "math sum" "math variance"))
-                                 ("random" . '("random" "random bool" "random chars" "random decimal" "random dice" "random integer" "random uuid"))
+(setq nu-keywords-regexp (regexp-opt nu-keywords 'words))
+(setq nu-type-regexp (regexp-opt nu-types 'words))
+(setq git-regexp (regexp-opt '("git" "git checkout" "git rebase" "git pull" "git commit" "git add" "git push")))
+(setq nu-core-regexp (regexp-opt '("alias" "debug" "def" "def-env" "describe" "do" "du" "echo" "error make" "export" "export def" "export def-env" "export env" "extern" "for" "help" "hide" "history" "if" "ignore" "let" "metadata" "module" "register" "source" "tutor" "use" "view-source")))
+(setq nu-filters-regexp (regexp-opt '("all?" "any?" "append" "collect" "columns" "compact" "default" "drop" "drop column" "drop nth" "each" "each group" "each window" "empty?" "every" "find" "first" "flatten" "get" "headers" "keep" "keep until" "keep while" "last" "length" "lines" "merge" "move" "par-each" "par-each group" "prepend" "range" "reject" "rename" "reverse" "roll" "roll down" "roll left" "roll right" "roll up" "rotate" "select" "shuffle" "skip" "skip until" "skip while" "sort-by" "uniq" "update" "update cells" "where" "wrap" "zip")))
+(setq nu-platform-regexp (regexp-opt '("ansi" "ansi gradient" "ansi strip" "clear" "input" "keybindings" "keybindings default" "keybindings list" "keybindings listen" "kill" "sleep" "term size")))
+(setq nu-system-regexp (regexp-opt '("benchmark" "exec" "ps" "run_external" "sys" "which")))
+(setq nu-strings-regexp (regexp-opt '("build-string" "char" "decode" "detect columns" "format" "nu-highlight" "parse" "size" "split" "split chars" "split column" "split row" "str" "str camel-case" "str capitalize" "str collect" "str contains" "str downcase" "str ends-with" "str find-replace" "str index-of" "str kebab-case" "str length" "str lpad" "str pascal-case" "str reverse" "str rpad" "str screaming-snake-case" "str snake-case" "str starts-with")))
+(setq nu-generators-regexp (regexp-opt '("cal" "seq" "seq date")))
+(setq nu-filesystem-regexp (regexp-opt '("cd" "cp" "load-env" "ls" "mkdir" "mv" "open" "rm" "save" "touch")))
+(setq nu-deprecated-regexp (regexp-opt '("dataframe" "insert" "nth" "pivot" "str to-datetime" "str to-decimal" "str to-int" "unalias")))
+(setq nu-date-regexp (regexp-opt '("date" "date format" "date humanize" "date list-timezone" "date now" "date to-table" "date to-timezone")))
+(setq nu-dataframe-regexp (regexp-opt '("dfr" "dfr aggregate" "dfr all-false" "dfr all-true" "dfr append" "dfr arg-max" "dfr arg-min" "dfr arg-sort" "dfr arg-true" "dfr arg-unique" "dfr column" "dfr concatenate" "dfr contains" "dfr count-null" "dfr count-unique" "dfr cumulative" "dfr describe" "dfr drop" "dfr drop-nulls" "dfr dtypes" "dfr filter-with" "dfr first" "dfr get" "dfr get-day" "dfr get-hour" "dfr get-minute" "dfr get-month" "dfr get-nanosecond" "dfr get-ordinal" "dfr get-second" "dfr get-week" "dfr get-weekday" "dfr get-year" "dfr group-by" "dfr is-duplicated" "dfr is-in" "dfr is-not-null" "dfr is-null" "dfr is-unique" "dfr join" "dfr last" "dfr melt" "dfr not" "dfr open" "dfr pivot" "dfr rename" "dfr rename-col" "dfr replace" "dfr replace-all" "dfr rolling" "dfr sample" "dfr set" "dfr set-with-idx" "dfr shape" "dfr shift" "dfr slice" "dfr sort" "dfr str-lengths" "dfr str-slice" "dfr strftime" "dfr take" "dfr to-csv" "dfr to-df" "dfr to-dummies" "dfr to-lowercase" "dfr to-nu" "dfr to-parquet" "dfr to-uppercase" "dfr unique" "dfr value-counts" "dfr with-column")))
+(setq nu-shells-regexp (regexp-opt '("enter" "exit" "shells")))
+(setq nu-env-regexp (regexp-opt '("env" "let-env" "with-env")))
+(setq nu-network-regexp (regexp-opt '("fetch" "url" "url host" "url path" "url query" "url scheme")))
+(setq nu-conversions-regexp (regexp-opt '("fmt" "into" "into binary" "into bool" "into datetime" "into filesize" "into int" "into string")))
+(setq nu-formats-regexp (regexp-opt '("from" "from csv" "from eml" "from ics" "from ini" "from json" "from ods" "from ssv" "from toml" "from tsv" "from url" "from vcf" "from xlsx" "from xml" "from yaml" "from yml" "to" "to csv" "to html" "to json" "to md" "to toml" "to tsv" "to url" "to xml" "to yaml")))
+(setq nu-viewers-regexp (regexp-opt '("grid" "table")))
+(setq nu-default-regexp (regexp-opt '("group-by" "hash md5" "hash sha256" "into decimal" "path" "path basename" "path dirname" "path exists" "path expand" "path join" "path parse" "path relative-to" "path split" "path type" "reduce" "split-by" "str substring" "str trim" "str upcase" "transpose" "version")))
+(setq nu-hash-regexp (regexp-opt '("hash" "hash base64")))
+(setq nu-math-regexp (regexp-opt '("math" "math abs" "math avg" "math ceil" "math eval" "math floor" "math max" "math median" "math min" "math mode" "math product" "math round" "math sqrt" "math stddev" "math sum" "math variance")))
+(setq nu-random-regexp (regexp-opt '("random" "random bool" "random chars" "random decimal" "random dice" "random integer" "random uuid")))
 
-   ))
+
+(setq nu-font-lock-keywords
+      `(
+        (,nu-type-regexp . font-lock-type-face)
+        (,nu-constant-regexp . font-lock-constant-face)
+
+        (,git-regexp  . font-lock-builtin-face)
+
+        (,nu-core-regexp . font-lock-builtin-face)
+        (,nu-filters-regexp . font-lock-builtin-face)
+        (,nu-platform-regexp . font-lock-builtin-face)
+        (,nu-system-regexp . font-lock-builtin-face)
+        (,nu-strings-regexp . font-lock-builtin-face)
+        (,nu-generators-regexp . font-lock-builtin-face)
+        (,nu-filesystem-regexp . font-lock-builtin-face)
+        (,nu-deprecated-regexp . font-lock-builtin-face)
+        (,nu-date-regexp . font-lock-builtin-face)
+        (,nu-dataframe-regexp .  font-lock-builtin-face)
+        (,nu-shells-regexp . font-lock-builtin-face)
+        (,nu-env-regexp . font-lock-builtin-face)
+        (,nu-network-regexp . font-lock-builtin-face)
+        (,nu-conversions-regexp .  font-lock-builtin-face)
+        (,nu-formats-regexp . font-lock-builtin-face)
+        (,nu-viewers-regexp . font-lock-builtin-face)
+        (,nu-default-regexp . font-lock-builtin-face)
+        (,nu-hash-regexp . font-lock-builtin-face)
+        (,nu-math-regexp . font-lock-builtin-face)
+        (,nu-random-regexp . font-lock-builtin-face)
 
 
-(defconst nu-keywords
-  '("\\(let\\|for\\|def\\|if\\|else\\|export\\)"))
+        (,(rx symbol-start "def" (1+ space) (group (1+ (or word ?_ ?' "\"" ))))
+         (1  font-lock-function-name-face))
+        (,nu-keywords-regexp . font-lock-keyword-face)
 
-(defconst nu-highlights
-  '(;; Variable interpolation looks like "{{varname}}"
-    ("$[a-zA-Z-0-9 \"]*" . font-lock-variable-name-face)
-    ;; Variable assignment looks like "varname :="
-    ("\\([^ \n]*\\) *=" 1 font-lock-variable-name-face)
-    ;; Highlight variable interpolation in shell scripts like "${varname}"
-    ("\\${\\([^}\n]*\\)}" 1 font-lock-variable-name-face)
-    ;; Highlight rules like "rulename:"
-    ("^\\(@?\\)\\([^ @:\n]+\\).*:\\([^=\n]\\|$\\)"
-     (1 font-lock-negation-char-face)
-     (2 font-lock-function-name-face))
-    ;; TODO highlight arguments to rules. I would have done it, but it was hard so I gave up for now.
-    ))
+        ("$[a-zA-Z-0-9_\"]*" . font-lock-variable-name-face)
+        ("\\([^ \n]*\\) *=" 1 font-lock-variable-name-face)
 
-(defconst nu-font-lock-keywords
-  (append nu-keywords
-          nu-highlights))
+        ))
 
 (defconst nu-mode-syntax-table
   (let ((syntax-table (make-syntax-table)))
@@ -84,41 +104,9 @@
     (modify-syntax-entry ?- "w" syntax-table)
     ;; backticks are like quotes in shell
     (modify-syntax-entry ?` "\"" syntax-table)
-    syntax-table))
+    syntax-table)
 
-(defun nu-untab-region (N)
-  "Untab a selected region using `indent-rigidly'.
-Argument N number of untabs to perform"
-  (interactive "p")
-  (let ((begin (if (use-region-p)
-                 (region-beginning)
-                 (line-beginning-position)))
-        (end (if (use-region-p)
-                 (region-end)
-                 (line-end-position))))
-    (indent-rigidly begin end (* N -4))))
-
-;; from https://www.emacswiki.org/emacs/BackspaceWhitespaceToTabStop
-;; (which is licensed GPL 2 or later)
 (defvar nu-indent-offset 4 "My indentation offset.")
-(defun nu-backspace-whitespace-to-tab-stop ()
-  "Delete whitespace backwards to the next tab-stop, otherwise delete one character."
-  (interactive)
-  (if (or indent-tabs-mode
-          (region-active-p)
-          (save-excursion
-            (> (point) (progn (back-to-indentation)
-                              (point)))))
-      (call-interactively #'backward-delete-char-untabify)
-    (let ((movement (% (current-column) nu-indent-offset))
-          (p (point)))
-      (when (= movement 0) (setq movement nu-indent-offset))
-      ;; Account for edge case near beginning of buffer
-      (setq movement (min (- p 1) movement))
-      (save-match-data
-        (if (string-match "[^\t ]*\\([\t ]+\\)$" (buffer-substring-no-properties (- p movement) p))
-            (backward-delete-char (- (match-end 1) (match-beginning 1)))
-          (call-interactively #'backward-delete-char))))))
 
 (defun nu-indent-line ()
   "Indent bodies of rules by the previous indent, or by `tab-width'."
@@ -158,16 +146,14 @@ Argument N number of untabs to perform"
   (setq-local comment-start-skip "#+[ \t]*")
 
   ;; Tabs
-  (setq-local tab-width 4)
-  (setq-local tab-stop-list (number-sequence 0 120 4))
+  (setq-local tab-width 2)
+  (setq-local tab-stop-list (number-sequence 0 120 2))
 
   (when (boundp 'evil-shift-width)
     (setq-local evil-shift-width 4))
 
   ;; Indentation
-  (setq-local indent-line-function 'nu-indent-line)
-  (local-set-key (kbd "DEL") #'nu-backspace-whitespace-to-tab-stop)
-  (local-set-key (kbd "<backtab>") #'nu-untab-region))
+  (setq-local indent-line-function 'nu-indent-line))
 
 
 ;;; nu-mode.el ends here
